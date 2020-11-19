519506da8521cab46140fbef2fd81a9943f77360
Alex <alex31inq@gmail.com>
2020-11-19 21:45:24 +0300

    create html

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-var
-    gulp         = require('gulp'),
-    browserSync  = require('browser-sync'),
+const gulp = require('gulp');
+const browserSync  = require('browser-sync').create();
+const less = require('gulp-less');
 
-    less = require('gulp-less');
-
-gulp.task('browser-sync', function () {
-    browserSync({
-        server: { baseDir: '.' },
-        startPath: ''
-    });
-});
-
-gulp.task('less', function () {
+gulp.task('less', gulp.series(function () {
     return gulp.src('dev/less/main.less')
     .pipe(less())
     .pipe(gulp.dest('build/css'));
-});
+}));
 
-gulp.task('watch', function () {
-    gulp.watch('dev/less/*.less', ['less'], browserSync.reload);
-    gulp.watch('*.html', browserSync.reload);
-});
+gulp.task('build', gulp.series('less'));
 
-gulp.task('build', ['less']);
-
-gulp.task('default', ['watch', 'build', 'browser-sync']);
-
+gulp.task('default', gulp.series(function() {
+    browserSync.init({
+        server: { baseDir: './' },
+        startPath: 'main.html'
+    });
+    const html = gulp.watch('*.html');
+    html.on('change', function() {
+        console.log('html changed');
+        browserSync.reload();
+    })
+    const less = gulp.watch('dev/less/*.less');
+    less.on('change', gulp.series('less', function() {
+        console.log('less changed');
+        browserSync.reload();
+    }))
+}));
